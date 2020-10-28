42826525defa06443aabfe122e557b236fdf8e02
YAOWANAT DONGNGAM <yaowanat.yy@mail.kmutt.ac.th>
2020-10-29 06:32:33 +0700

    connect database to dropbox

---
+++ MyStomach/src/main/java/Servlet/MenuServlet.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import Entity.Menu;
+import static java.awt.SystemColor.menu;
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.util.Collections.list;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author yunxyinx
+ */
+public class MenuServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TestJpaMenuPU");
+        EntityManager em = emf.createEntityManager();
+
+        Query all = em.createNamedQuery("Menu.findAll");
+        Query name = em.createNamedQuery("Menu.findByFood");
+        //String dollar = request.getParameter("food");
+
+        String sql = " SELECT m FROM Menu m WHERE m.food like :food ";
+        Query food = em.createQuery(sql);
+        food.setParameter("food", "ข้าว%");
+
+        List<Menu> listName = food.getResultList();
+        for (Menu listMenu : listName) {
+            System.out.println(listMenu.getFood());
+            request.setAttribute("listMenu", listMenu);
+            
+        }
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/SetAlarm.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- MyStomach/src/main/webapp/SetAlarm.jsp
+++ MyStomach/src/main/webapp/SetAlarm.jsp
@@ -4,7 +4,9 @@
     Author     : yunxyinx
 --%>
 
+
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql" %>  
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -12,42 +14,33 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
     </head>
+
     <body>
-    <sql:setDataSouse var="" driver=""></sql:setDataSouse>
-    <sql:query datasouse="" var="result"></sql:query>
     <form action="SetAlarm" method="GET">
-        <table>
-            <tr>
-                <td><label for="appt" name="breakfast">Breakfast: </label>
-                    <input type="time" id="appt" name="time"></td>
-                <td><select name="foods" id="foods">
-                        <option type="dorpbox" name="foods" value="Apple"/>Apple
-                        <option type="dorpbox" name="foods" value="Rice"/>Rice
-                    </select></td>
-            </tr>
-            <tr>
-                <td><label for="appt" name="breakfast">Breakfast: </label>
-                    <input type="time" id="appt" name="time1"></td>
-                <td><select name="foods1" id="foods1">
-                        <option type="dorpbox" name="foods" value="Sushi"/>Sushi
-                        <option type="dorpbox" name="foods" value="Rice"/>Rice
-                    </select>
-                </td>
-                <td>
-                    <input type="number" name="meal"/>
-                </td>
-            </tr>
-            <tr>
-                <td><label for="appt" name="breakfast">Breakfast: </label>
-                    <input type="time" id="appt" name="time"></td>
-                <td><c:forEach var="row" items="${result.row}">
-                        <select name="foods" id="foods">
-                            <option type="dorpbox" name="foods" ><c:out value="${row.foods}"/></option>
-                        </select></c:forEach></td>
-            </tr>
-        </table>
-        <p>Calories: <input type="text" name="day"/></p>
+
+                <c:forEach var="i" begin="0" end="2">
+                <label for="appt" name="breakfast">Breakfast: </label>
+                <input type="time" valu e=i id="appt" name="time">
+                
+                <select name="foods"${i} id="foods">
+                    <option type="dorpbox" name="foods" value="Apple"/>Apple
+                    <option type="dorpbox" name="foods" value="Rice"/>Rice
+                </select>
+                    <br>
+        </c:forEach>
+        <label for="appt" name="breakfast">Break: </label>
+        <input type="time" id="appt" name="time">
+        
+        <from action="MenuServlet" method="get">
+        <select name="menu">
+            <option value="${listMenu.menuID}">${listMenu.food}</option>
+            <option value="${listMenu.menuID}">${listMenu.food}</option>
+            <option value="${listMenu.menuID}">${listMenu.food}</option>
+            </select>
+
+        <br>
         <input type="submit"/>
+        </from>
 
     </form>
 </body>
